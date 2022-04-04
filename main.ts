9b7bc9d969b15e01b4bf6cfcd7918004ceb69762
Jingles05 <102975990+jingles05@users.noreply.github.com>
2022-04-04 08:48:25 -0600

    added more stuff

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 3; index++) {
+        basic.showString("BYE!")
+    }
+})
 basic.showArrow(ArrowNames.North)
 basic.showIcon(IconNames.Target)
 basic.showString("HELLO!")
